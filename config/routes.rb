24822d234087c74a00111fc488c57e95359b62c0
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-19 17:10:59 -0600

    seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources: :users, only: [:index, :create, :update, :destroy]
-  resources: :posts, only: [:index, :create, :update, :destroy]
-  resources: :comments, only: [:index, :create, :update, :destroy]
-  resources: :addresses, only: :index
-  resources: :events, only: :index
+  resources :users, only: [:index, :create, :update, :destroy]
+  resources :posts, only: [:index, :create, :update, :destroy]
+  resources :comments, only: [:index, :create, :update, :destroy]
+  resources :addresses, only: :index
+  resources :events, only: :index
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   
