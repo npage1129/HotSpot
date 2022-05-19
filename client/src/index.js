b4bda6ca11b5da8883a1849d8b8be99b5665c40c
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-19 12:29:42 -0600

    better

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Container, Header, List } from "semantic-ui-react";
 import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.render(
 ( <BrowserRouter>
