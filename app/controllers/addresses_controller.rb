f2179c78f0839ee302408c97408b3009107eeb26
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-19 11:19:56 -0600

    errors

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,6 @@
+class AddressesController < ApplicationController
+
+    def index
+        render json: Address.all
+    end
+end
---
+++ app/models/event.rb
@@ -0,0 +1,5 @@
+class Event < ApplicationRecord
+    has_many: :addresses
+    has_many: :posts, dependent: :destroy
+
+end
