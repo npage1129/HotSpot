d07a1e9492b28e2f517c9d48e59437e349986919
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-05-19 15:34:53 -0600

    header

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './Header.css';
 import { Grid } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 
 
 
-const Header = () => {
-    <div>hello!</div>
+function Header(){
+    
 return (
     <div>
         <div className="navBar">
 <Grid container>
     <Grid item xs={0}></Grid>
-    <img className="logoNav" src={'https://lh3.googleusercontent.com/sRrBGmDHDnR28LqMo7iDdUSxG-uXoFS7lyf0FwIAHV_8iR05kBvhE-JFoM5hlwLqtC2Z9TNrN3KLpIM3zykwKld69d-tR0EqiyEoAPY_XLlR6T7cHzd65YOgDGmfdIA5oqW5Bmc2w2s=w2400'}/>
+    <img className="logoNav" src={'https://lh3.googleusercontent.com/sRrBGmDHDnR28LqMo7iDdUSxG-uXoFS7lyf0FwIAHV_8iR05kBvhE-JFoM5hlwLqtC2Z9TNrN3KLpIM3zykwKld69d-tR0EqiyEoAPY_XLlR6T7cHzd65YOgDGmfdIA5oqW5Bmc2w2s=w2400'} alt={'hello'}/>
     <Grid item xs={4}>
    <input className= "searchBar" text="text" placeholder="Search"/>
    <button>Submit</button>
@@ -20,12 +22,13 @@ return (
     <Grid item xs={2}></Grid>
 
         <div className ="icons">
-
+<LogoutOutlinedIcon className ="Home"/>
+<SendOutlinedIcon className="messages"/>
 
         <div className = "avatar">
-    <Avatar/>
+    <Avatar/> 
     
-    </div>
+</div>
 
     </div>
 </Grid>
